7e2110bb16f9b6c7b5c1536942a8452f16e5bf1f
NicolasPicardi <signos97@gmail.com>
2020-11-08 12:43:33 -0300

    Delete Post

--- src/context/BlogContext.tsx
+++ src/context/BlogContext.tsx
@@ -6,7 +6,9 @@ import createDataContext from './createDataContext'
 const blogReducer = (state: iBlogPost[], action:any) => {
 	switch (action.type) {
 		case 'ADD_POST':
-			return [...state, {title: `Blog post #${state.length}`}]
+			return [...state, {title: `Blog post #${state.length}`, id: Date.now()}]
+		case 'DELETE_POST':
+			return state.filter(post => post.id !== action.payload)
 		default:
 			return [...state]
 	}
@@ -16,6 +18,11 @@ const actions = {
 	addBlogPost(dispatch: React.Dispatch<any>){
 		return () => {
 			dispatch({type: 'ADD_POST'})
+		}
+	},
+	deleteBlogPost(dispatch: React.Dispatch<any>){
+		return (id:number) => {
+			dispatch({type: 'DELETE_POST', payload: id})
 		}
 	}
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
-import { FlatList } from 'react-native-gesture-handler'
+import { Feather } from '@expo/vector-icons'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 import { useBlog } from '../hooks/useBlog'
 import { iBlogPost } from '../types/blogTypes'
 
@@ -13,7 +14,16 @@ const Home = () => {
       <FlatList
         data={state}
         keyExtractor={(state:iBlogPost) => state.title}
-        renderItem={({item}) => <Text>{item.title}</Text>}
+        renderItem={({item}) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <TouchableOpacity onPress={() => actions.deleteBlogPost(item.id)}>
+                <Feather name="trash" style={styles.icon} />
+              </TouchableOpacity>
+            </View>
+          )}
+        }
       />
     </View>
   )
@@ -21,4 +31,19 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  row:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+})
